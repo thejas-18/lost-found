4db11116fb00eb2fbd5e44ea8067fdeffe39301e
thejas <gowdathejas999@gmail.com>
2026-03-22 20:05:54 +0530

    fixed huggingface model path

--- download_model.py
+++ download_model.py
@@ -2,6 +2,6 @@ from sentence_transformers import SentenceTransformer
 
 model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
 
-model.save("model/all-MiniLM-L6-v2")
+model.save("all-MiniLM-L6-v2")
 
 print("Model saved locally!")
